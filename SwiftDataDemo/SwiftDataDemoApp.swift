18d2f799580fc9f474aacd268c25590c04b963ee
evilicebergs <voiced.fair-0a@icloud.com>
2024-10-28 18:08:30 +0200

    Initial Commit

---
+++ SwiftDataDemo/SwiftDataDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataDemoApp.swift
+//  SwiftDataDemo
+//
+//  Created by Artem Golovchenko on 2024-10-28.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
